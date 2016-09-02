29b9ab63908ef3b85557b0d8e505d92221e16b37
Bert Jan Schrijver <bertjan@revolution.nl>
2016-09-02 21:59:04 +0200

    Initial project setup.

---
+++ src/main/java/nl/revolution/example/HelloWorldController.java
@@ -0,0 +1,17 @@
+package nl.revolution.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
