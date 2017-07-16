32d5627517944deb7d1b6d02fef98ee6a4d0dfb7
arun <er.arunkumar@live.com>
2017-07-16 19:44:12 +0530

    Merge branch 'master' of https://github.com/erarunkumar/spring-boot-sample
    # Conflicts:
    #	Jenkinsfile

--- scripts/spring-boot-sample.sh
+++ scripts/spring-boot-sample.sh
@@ -2,4 +2,4 @@
 date
 ls -l target/spring-boot-sample.jar
 echo "running spring-boot-sample....."
-java -jar target/spring-boot-sample.jar > /tmp/spring-boot-sample.log 2>&1 &
+/root/software/java_1.8.131/jdk1.8.0_131/bin/java -jar target/spring-boot-sample.jar > /tmp/spring-boot-sample.log 2>&1 &
